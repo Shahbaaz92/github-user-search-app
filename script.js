256fd64225bd039ef709e96b995d183fe27b2e55
Shahbaaz <59239856+Shahbaaz92@users.noreply.github.com>
2023-12-07 13:14:51 +0530

    Html and css updated

---
+++ script.js
@@ -0,0 +1,6 @@
+const stats = document.getElementById("stats");
+const searchBox = document.getElementById("search-box");
+const toggle = document.getElementById("toggle");
+const bodyBox = document.getElementById("body-box");
+
+toggle.addEventListener(function () {});
